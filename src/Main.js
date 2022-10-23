036fe731dffeb497309cd3ae6c02c531fb553677
Matheus <matheus.figueiredo1999@yahoo.com>
2022-10-23 11:22:03 -0400

    first commit

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import './Main.css';
+import Sorteador from './Sorteador';
+
+function Main() {
+    const [resultado, setResultado] = useState('');
+
+    const resultadoAtualizado = () => {
+        setResultado(Sorteador());
+    }
+
+    return (
+        <div className='Sorteio'>
+            <h2>Sorteie o número: </h2>
+            <button onClick={resultadoAtualizado}>Sortear</button>
+            <h3>{resultado}</h3>
+        </div>
+    );
+}
+
+export default Main;
