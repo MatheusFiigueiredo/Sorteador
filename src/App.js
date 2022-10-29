a7d2411596845d1fb63e1c258312496168520582
Matheus <matheus.figueiredo1999@yahoo.com>
2022-10-29 19:28:26 -0400

    Adição do relogio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import Main from "./componentes/Main";
-
+import { useEffect, useState } from "react";
+import Main from "./componentes/Main/Main";
+import "./componentes/Relogio/Relogio.css"
 
 function App() {
+  const [Hora, setHora] = useState(23);
+    const [Minuto, setMinuto] = useState(59);
+    const [Segundo, setSegundo] = useState(59);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSegundo(Segundo + 1);
+            if (Segundo === 59) {
+                setSegundo(0);
+                setMinuto(Minuto + 1);
+                if (Minuto === 59) {
+                    setMinuto(0);
+                    setHora(Hora + 1);
+                    if (Hora === 23 && Minuto === 59 && Segundo === 59) {
+                        setHora(0);
+                        setMinuto(0);
+                        setSegundo(0);
+                    }
+                }
+            }
+        }, 1000)
+        return () => clearInterval(interval);
+    })
   return (
     <div className="App">
       <Main/>
+      <div className="Relogio">
+        <h2>{Hora}:{Minuto}:{Segundo}</h2>
+      </div>
     </div>
   );
 }
